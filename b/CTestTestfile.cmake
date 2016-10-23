f7a39638b550158cd1d147b469410b53ad0ae49d
Christopher Wood <christopherwood07@gmail.com>
2016-10-23 16:54:53 -0700

    Remove the build directory

--- b/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ivan/Desktop/projects/ccvpn/ccvpn
-# Build directory: /home/ivan/Desktop/projects/ccvpn/ccvpn/b
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(EmptyTest, "echo" "OK")
-subdirs(gateway)
