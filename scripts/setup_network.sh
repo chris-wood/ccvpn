0e079eba1ca2d7480823488ac76e09ac9b49ae13
ivanolive <ivanoliv.1@gmail.com>
2016-12-21 20:43:52 -0800

    results

--- scripts/setup_network.sh
+++ scripts/setup_network.sh
@@ -7,8 +7,8 @@ NUMBER_PRODUCERS=$1
 
 echo "\n\nUsage: sh setup_network.sh <NUMBER_PRODUCERS>\n\n"
 
-../b/gateway/ccnx/forwarder/athena/command-line/athena/athena_private ccnx:/gateway /tmp/key.pub /tmp/key.sec &
-../b/gateway/ccnx/forwarder/athena/command-line/athena/athena_gateway ccnx:/foo 1 ccnx:/producer ccnx:/gateway /tmp/key.pub tcp://localhost:9695/name=tunnel/local=false -c tcp://localhost:9696/listener/local=false &
+../b/gateway/ccnx/forwarder/athena/command-line/athena/athena_private ccnx:/gateway /tmp/key.pub /tmp/key.sym &
+../b/gateway/ccnx/forwarder/athena/command-line/athena/athena_gateway ccnx:/foo 1 ccnx:/producer ccnx:/gateway /tmp/key.sym tcp://localhost:9695/name=tunnel/local=false -c tcp://localhost:9696/listener/local=false &
 sleep 1s
 sh start_producers.sh ../b/ccnxVPN_Server ccnx:/producer $NUMBER_PRODUCERS
 
